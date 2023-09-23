2dbd820ac4dd77deefe7f3053f2106fb9973505b
Javier Urbina <htorres332.jt@gmail.com>
2023-09-23 19:15:32 -0300

    Feature contact form enable!

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,7 +1,8 @@
 import { Button, Grid, TextField } from "@mui/material"
 import { useForm } from "../hooks/useForm"
 import { useTranslation } from "react-i18next"
-
+import emailjs from '@emailjs/browser';
+import SendIcon from '@mui/icons-material/Send';
 
 const initialFormContact = {
     name: "",
@@ -12,66 +13,71 @@ const initialFormContact = {
 
 export const ContactForm = () => {
     const [t] = useTranslation("global")
-    const {name, phone, email, message, onInputChange} = useForm(initialFormContact);    
+    const { name, phone, email, message, onInputChange } = useForm(initialFormContact);
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log({name, phone, email, message});
+        emailjs.send("service_ljans2d", "template_xo8outb", {
+            user_name: name,
+            message: message,
+            user_phone: phone,
+            user_email: email,
+        }, "AL_rLlhZZ34GoeVHC");
     }
 
     return (
-        <form onSubmit={handleSubmit} style={{display:"flex", alignItems:"center", width: "100%"}}>
+        <form onSubmit={handleSubmit} style={{ display: "flex", alignItems: "center", width: "100%" }}>
             <Grid container spacing={2}>
                 <Grid item xs={4}>
                     <TextField
-                    label={t("contactform.name")}
-                    type="text"
-                    fullWidth
-                    color="warning"
-                    name='name'
-                    value={name}
-                    onChange={onInputChange}
+                        label={t("contactform.name")}
+                        type="text"
+                        fullWidth
+                        color="warning"
+                        name='name'
+                        value={name}
+                        onChange={onInputChange}
                     // helperText={emailValid}
                     // error={!!emailValid && formSubmitted}
                     />
                 </Grid>
                 <Grid item xs={4}>
                     <TextField
-                    label={t("contactform.phone")}
-                    type="text"
-                    fullWidth
-                    color="warning"
-                    name='phone'
-                    value={phone}
-                    onChange={onInputChange}
+                        label={t("contactform.phone")}
+                        type="text"
+                        fullWidth
+                        color="warning"
+                        name='phone'
+                        value={phone}
+                        onChange={onInputChange}
                     />
                 </Grid>
                 <Grid item xs={4}>
                     <TextField
-                    label={t("contactform.email")}
-                    type="text"
-                    fullWidth
-                    color="warning"
-                    name='email'
-                    value={email}
-                    onChange={onInputChange}
+                        label={t("contactform.email")}
+                        type="text"
+                        fullWidth
+                        color="warning"
+                        name='email'
+                        value={email}
+                        onChange={onInputChange}
                     />
                 </Grid>
                 <Grid item xs={12}>
                     <TextField
-                    label={t("contactform.message")}
-                    type="text"
-                    fullWidth
-                    color="warning"
-                    multiline
-                    rows={7}
-                    name='message'
-                    value={message}
-                    onChange={onInputChange}
+                        label={t("contactform.message")}
+                        type="text"
+                        fullWidth
+                        color="warning"
+                        multiline
+                        rows={7}
+                        name='message'
+                        value={message}
+                        onChange={onInputChange}
                     />
                 </Grid>
                 <Grid item container direction={"row"} justifyContent={"end"}>
-                    <Button type='submit' variant="contained" sx={{ color: "inherit", backgroundColor: "#Bf7625" }}>
+                    <Button type='submit' variant="contained" endIcon={<SendIcon />} sx={{ color: "inherit", backgroundColor: "#Bf7625" }}>
                         {t("contactform.send")}
                     </Button>
                 </Grid>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,20 @@
+import { Pagination } from "@mui/material"
 import { PageLayout } from "../layout/PageLayout"
 
 export const About = () => {
     return (
-        <PageLayout justifyContent="end">
-            <h1>Hola mundo desde about</h1>
+        <PageLayout justifyContent="start" alignItems="start">
+            <h6>Historia Personal</h6>
+            <h6>Mi pasión</h6>
+            <h6>Experiencia y logros</h6>
+            <h6>Valores y etica de trabajo</h6>
+            <h6>Habilidades clave</h6>
+            <h6>Testimonios o recomendaciones</h6>
+            <h6>Educación y formación</h6>
+            <h6>Intereses personales</h6>
+            <h6>Estilo personal</h6>
+            <h6>Fotos y multimedia</h6>
+            <Pagination count={10} />
         </PageLayout>
     )
 }

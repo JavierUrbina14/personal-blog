11c7b974a6308587fb8efb75a5400c20730d575c
Javier Urbina <htorres332.jt@gmail.com>
2023-10-02 00:17:35 -0300

    bug fix contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,8 +7,8 @@ export const Contact = () => {
   const [t] = useTranslation("global")
   return (
     <PageLayout>
-      <Grid display="flex" justifyContent="center" margin={"4rem 0"}>
-        <Typography variant="h4" fontWeight={"bold"} fontSize={{ xs: "1.6rem", md:"2rem", lg:"2rem", xl: "2.5rem" }} className="degradado">{t("contact.title")}</Typography>
+      <Grid display="flex" justifyContent="center" margin={{lg:"1.5rem 0", xl:"4rem 0"}}>
+        <Typography variant="h4" fontWeight={"bold"} fontSize={{ xs: "1.6rem", md:"1.8rem", lg:"2rem", xl: "2.5rem" }} className="degradado">{t("contact.title")}</Typography>
       </Grid>
       <Grid container justifyContent={"center"}>
         <Grid item display={"flex"} sx={{ flexDirection: { xs: "column-reverse", md: "row" }, margin: { xs: "0 1rem", md: "0 3rem", lg: "0 5rem", xl: "1rem 10rem" }, gap: "5rem", height: "100%" }}>
